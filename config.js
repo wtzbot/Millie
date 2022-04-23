faadeb2b47c05c4a63054368305a882ad3212784
wtzbot <96922881+wtzbot@users.noreply.github.com>
2022-04-23 11:32:28 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 let fs = require('fs')
-global.owner = ['918113921898'] // Put your number here
+global.owner = ['94769228163'] // Put your number here
 global.mods =  JSON.parse(fs.readFileSync('./src/moderator.json'))// Moderator?
 global.prems = JSON.parse(fs.readFileSync('./src/premium.json')) // Premium users do not need a limit
 global.tod = JSON.parse(fs.readFileSync('./src/tod.json'))
@@ -40,7 +40,7 @@ global.wait = '_*Please Wait..*_'
 global.eror = '_*Server Error*_'
 global.fla = 'https://github.com/neera-j/Bot-Files/blob/main/images/millie.jpg'
 
-global.watermark = '© ᴍɪʟʟɪᴇ'
+global.watermark = 'ᴳᵉⁿᵉʳᵃᵗᵉᵈ ᴮʸ ༒☬ꪶ࿋྄ིᤢꫂÇhͥαmͣïͫ†h༻☬༒'
 
 global.multiplier = 69 // The higher it is, the harder it is to level up
 
